60a3213bb4cf4e246c35386276bbbd59a538f297
Mateusz Gutowski <mateusz280798@gmail.com>
2024-11-09 20:31:00 +0100

    Init OT

---
+++ OpenTelemetry/RestApi/RestApi/OpenTelemetryExtenions.cs
@@ -0,0 +1,51 @@
+﻿using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+namespace RestApi;
+
+internal static class OpenTelemetryExtenions
+{
+    public static IHostApplicationBuilder ConfigureOpenTelemetry(this IHostApplicationBuilder builder)
+    {
+        var tracingOtlpEndpoint = builder.Configuration["OTEL_EXPORTER_OTLP_ENDPOINT"];
+        var otel = builder.Services.AddOpenTelemetry();
+
+        // Configure OpenTelemetry Resources with the application name
+        otel.ConfigureResource(resource => resource
+            .AddService(serviceName: builder.Environment.ApplicationName));
+
+        // Add Metrics for ASP.NET Core and our custom metrics and export to Prometheus
+        otel.WithMetrics(metrics => metrics
+            // Metrics provider from OpenTelemetry
+            .AddAspNetCoreInstrumentation()
+            // Metrics provides by ASP.NET Core in .NET 8
+            .AddMeter("Microsoft.AspNetCore.Hosting")
+            .AddMeter("Microsoft.AspNetCore.Server.Kestrel")
+            .AddPrometheusExporter());
+
+        // Add Tracing for ASP.NET Core and our custom ActivitySource and export to Jaeger
+        otel.WithTracing(tracing =>
+        {
+            tracing.AddAspNetCoreInstrumentation();
+            tracing.AddHttpClientInstrumentation();
+            if (tracingOtlpEndpoint != null)
+            {
+                tracing.AddOtlpExporter(otlpOptions =>
+                {
+                    otlpOptions.Endpoint = new Uri(tracingOtlpEndpoint);
+                    otlpOptions.Protocol = OpenTelemetry.Exporter.OtlpExportProtocol.HttpProtobuf;
+                });
+            }
+        });
+
+        return builder;
+    }
+    public static WebApplication UseOpenTelemetry(this WebApplication builder)
+    {
+        builder.MapPrometheusScrapingEndpoint();
+        builder.UseOpenTelemetryPrometheusScrapingEndpoint();
+
+        return builder;
+    }
+}
